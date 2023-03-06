49177820c3f649aef119c7034990758b321dd04a
melih345 <52869185+melih345@users.noreply.github.com>
2023-03-06 04:04:09 +0300

    Add files via upload

---
+++ ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTA4MTI5MjcwNDI0NTM3MDg4MQ.GgkE0l.9Ol7o9b-TRp4mUYJDXInjHIzBkNEut2nDtP6dM",  // lrows token
+    PREFIX: process.env.PREFIX || ".", // lrows prefix
+    SAHİP: process.env.SAHİP || "180341615536242688", //lrows id
+}
